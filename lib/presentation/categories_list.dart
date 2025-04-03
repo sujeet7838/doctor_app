649767b4935c196c6f399a6e4d620bdf4fe49618
sujeet7838 <sujeet.bpl@gmail.com>
2025-04-03 18:51:04 +0530

    searchList nad doctors list integration

---
+++ lib/presentation/categories_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoriesList extends StatelessWidget {
+  const CategoriesList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
